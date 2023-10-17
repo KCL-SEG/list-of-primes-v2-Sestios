db6bb0b2a3938c1c4cb4e1f33bf3fb87c293a2bc
Yusuf Yusuf <yusuf.1.yusuf@kcl.ac.uk>
2023-10-17 01:11:21 +0100

    Solution for Exercise 1.4

--- primes.py
+++ primes.py
@@ -1,6 +1,21 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+import math
 
 def primes(number_of_primes):
+    if number_of_primes<=0:
+        raise ValueError()
     list = []
+    prime_index, counter, prime_break = 0, 2, False
+    while(prime_index < number_of_primes):
+        prime_break = True
+        for j in range(2, math.floor(math.sqrt(counter)) + 1):
+            if (counter%j == 0):
+                prime_break = False
+                break
+        if(prime_break):
+            list.append(counter)
+            prime_index+=1
+        counter+=1
+
     return list
